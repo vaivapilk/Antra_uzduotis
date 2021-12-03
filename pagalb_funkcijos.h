cf305e7a141c113790aa1c856d829389f3b4bc6b
vaivapilk <56174822+vaivapilk@users.noreply.github.com>
2021-12-03 17:26:26 +0200

    Add files via upload

---
+++ pagalb_funkcijos.h
@@ -0,0 +1,20 @@
+
+#pragma once
+#include "Studentas.h"
+
+extern std::ifstream f;
+extern vector<Studentas> vnesimoke2;
+extern list<Studentas> lnesimoke2;
+
+void generavimas(int K);
+int words_in_a_string(char* str);
+int lis_failo(list<Studentas>& grupe);
+int vis_failo(vector<Studentas>& grupe);
+void ldalinimas(char uzk, list<Studentas>& grupe, list<Studentas>& moksliukai, list<Studentas>& nesimoke);
+void vdalinimas(char uzk, vector<Studentas>& grupe, vector<Studentas>& moksliukai, vector<Studentas>& nesimoke);
+void ldalinimas2(char uzk, list<Studentas>& grupe);
+void vdalinimas2(char uzk, vector<Studentas>& grupe);
+void lisvedimas_i_faila(char uzk, string falo_vardas, list<Studentas>& studentai);
+void visvedimas_i_faila(char uzk, string falo_vardas, vector<Studentas>& studentai);
+
+
---
+++ vart_ivedimas.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Studentas.h"
+#include <regex>
+using std::regex;
+
+void clearInput();
+void tikrinam_regex(int& x);
+void pats_ivesiu(int stud_kiek, vector<Studentas>& vgrupe, list<Studentas>& lgrupe);
